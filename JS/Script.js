e328766578c59f143dec9a20290d89263bced627
Draguisada <draguisada@gmail.com>
2024-09-29 09:37:39 -0300

    Adicionado Sacramento Titulo
    TCHAU JAVA NÃO AGUENTO MAIS VOCÊ

--- JS/Script.js
+++ JS/Script.js
@@ -1,9 +1,31 @@
-function goto(p) {
-    if (p == 1) {
-        window.scrollTo({
-            top: 200,
-            left: 0,
-            behavior: "smooth",
-        })
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    opacity = Math.min(scroll, 526);
+    
+    if (opacity < 526) {
+        changeNavOpacity(opacity);
     }
+
+});
+
+nav = document.querySelector('nav');
+
+function changeNavOpacity(opacity) {
+
+    opacity = opacity*70/526;
+
+    nav.style.backgroundColor = 'rgba(13, 6, 1,' + opacity/100 + ')';
+}
+
+
+scroll(-2000, 0)
+
+function goto(elem) {
+    procura = document.getElementById(elem);
+    scrollToY = Math.max(procura.scrollHeight, procura.offsetTop);
+    if (elem == "tupaciguara") {
+        scrollToY = scrollToY - 200;
+    }
+
+    scroll({left:0, top:scrollToY, behavior: 'smooth'});
 }
