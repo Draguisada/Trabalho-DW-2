0940b69bdd7205b844f93fe5b9898e287b89a1fa
Draguisada <draguisada@gmail.com>
2024-10-04 18:41:09 -0300

    Araguari feita

--- JS/Script.js
+++ JS/Script.js
@@ -1,21 +1,51 @@
 nav = document.querySelector('nav');
 nav.style.display = 'None';
 
+var showNavMin = 400;
+
+const icone = document.getElementById('icon');
+const fundo_araguari = document.getElementsByClassName('araguari-header-titulo');
+
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
+    // console.log('ScrollY valor')
+    // console.log(scroll);
     
-    opacity = Math.min(scroll, 1000);
-    
-    if (opacity < 1000) {
-        nav.style.display = 'None';
-    } else if (opacity < 1550 || opacity > 1000) {
-        nav.style.display = 'Block'
-        changeNavOpacity(opacity);
+    // Mudar o icone da página dependendo do segmento (Oq eu to fazendo o site nem ta pronto)
+    scrollIcon = scroll;
+    if (scrollIcon < 1500) {
+        icone.href='Imagens/tapuciguara/poker-cards.png';
+    } else if (scrollIcon < 2800) {
+        icone.href='Imagens/Sacramento/T.webp';
+    } else if (scrollIcon < 3250) {
+        icone.href='Imagens/Araguari/Indio.png';
     }
 
+
+    // Mudar opacidade da Nav com o scroll
+    scrollNav = scroll - showNavMin/2
+    opacityNav = Math.min(scrollNav, 1000);
+    if (opacityNav < showNavMin) {
+        nav.style.display = 'None';
+    } else if (opacityNav < 1000 || opacityNav > showNavMin) {
+        nav.style.display = 'Block';
+        changeNavOpacity(opacityNav);
+    }
+
+    // Parallax para o titulo de Araguari
+    if (scroll > 2250) {
+        scrollArag = scroll;
+        var paralax = fundo_araguari[0].offsetTop - scrollArag;
+        paralax = 1000-paralax
+        // console.log('Fundo - paralax')
+        // console.log(paralax);
+        paralax = Math.min(Math.max(Math.ceil(paralax*45/624), 25), 45);
+        // console.log('Regra de 3')
+        // console.log(paralax);
+        fundo_araguari[0].style.backgroundPositionY = paralax + '%'
+    }
+    
 });
-
-
 
 function changeNavOpacity(opacity) {
 
@@ -31,11 +61,13 @@ function goto(elem) {
     procura = document.getElementById(elem);
     scrollToY = Math.max(procura.scrollHeight, procura.offsetTop);
     if (elem == "tupaciguara") {
-        scrollToY = scrollToY - 200;
+        scrollToY = scrollToY - 180;
     }
     if (elem == "sacramento") {
-        scrollToY = scrollToY - 350;
+        scrollToY = scrollToY - 320;
     }
+
+    scrollToY = scrollToY - 30;
 
     scroll({left:0, top:scrollToY, behavior: 'smooth'});
 }
